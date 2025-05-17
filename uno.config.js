19cdb8267a951e0f1eb3da9cae00946217bcb5ec
bryan hsiao <siao.bryan@gmail.com>
2025-05-17 12:00:28 +0800

    完成 github page 功能修改

--- uno.config.js
+++ uno.config.js
@@ -3,13 +3,5 @@ import presetAttributify from '@unocss/preset-attributify'
 import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
-  presets: [presetMini(), presetAttributify(), presetIcons()],
-  shortcuts: [
-    // 主題：企業配色
-    ['theme-business', 'text-gray-800 bg-white'],
-    ['theme-primary', 'text-blue-600'],
-    ['theme-secondary', 'text-yellow-500'],
-
-    // （之後可以加 theme-fresh, theme-dark，不急）
-  ],
+  presets: [presetMini(), presetAttributify(), presetIcons()]
 })
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 
+const isGithubPages = process.env.DEPLOY_ENV === 'gh-pages'
+
 export default defineConfig({
+  base: isGithubPages ? '/portal/' : './',
   plugins: [
     react(),
     UnoCSS(),
-  ],
-  base: './',  // 這行是重點
+  ]
 })
